b63f68f006bd2f1a37c3c78be7ef92d9e0e2f43e
luckyplanet42 <luckyplanet@protonmail.com>
2021-03-01 21:25:32 +0100

    refactor: Update

--- src/widgets/Menu/icons/Logo.tsx
+++ src/widgets/Menu/icons/Logo.tsx
@@ -10,7 +10,11 @@ const Logo: React.FC<LogoProps> = ({ isDark, ...props }) => {
   const textColor = isDark ? "#FFFFFF" : "#000000";
   return (
     <Svg viewBox="0 0 205 52" {...props}>
-      <image width="205" height="52" href={isDark ? "/images/rockswap/logoTextDark.svg" : "/images/rockswap/logoTextLight.svg"} />
+      <image
+        width="205"
+        height="52"
+        href={isDark ? "/images/rockswap/logoTextDark.svg" : "/images/rockswap/logoTextLight.svg"}
+      />
     </Svg>
   );
 };
